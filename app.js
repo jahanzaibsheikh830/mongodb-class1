72d26ca899d61f590689b00181f0793502d51762
Muhammad zubait <zubairsaleem1210@gmail.com>
2021-01-05 16:33:59 +0500

    changes

---
+++ app.js
@@ -0,0 +1,18 @@
+function signup(){
+    const Http = new XMLHttpRequest();
+    var url = "http://localhost:3000/signup"
+    Http.open("POST", url);
+    Http.setRequestHeader("Content-Type", "application/json");
+    Http.send(JSON.stringify({
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        phone: document.getElementById('phone').value,
+        gender: document.getElementById('gender').value,
+    }));
+
+    Http.onreadystatechange = (e) => {
+        alert(Http.responseText)
+    }
+return false  
+}
